f96eddc6cddbe64ebd3f64e77379218dcd175163
nrewankar <66852090+nrewankar@users.noreply.github.com>
2020-07-10 18:57:17 -0400

    Add files via upload

---
+++ Register Page/Register Page/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Register Page
+//
+//  Created by Owner1 on 2020-07-10.
+//  Copyright © 2020 Reserv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var NameTextfield: UITextField!
+    
+    @IBOutlet weak var AddressTextField: UITextField!
+    
+    @IBOutlet weak var BirthdayTextfield: UITextField!
+    
+    @IBOutlet weak var UsernameTextfield: UITextField!
+    
+    @IBOutlet weak var PasswordTextfield: UITextField!
+    
+    @IBOutlet weak var SignUpButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
