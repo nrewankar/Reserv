c1f6b9f1f6ffebf12bced7d725cf168f33a58b75
nrewankar <66852090+nrewankar@users.noreply.github.com>
2020-07-19 08:33:01 -0400

    Add files via upload

---
+++ SettingsPage/SettingsPage/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  SettingsPage
+//
+//  Created by Owner1 on 2020-07-18.
+//  Copyright © 2020 Reserv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var AccountButton: UIButton!
+    
+    @IBOutlet weak var PersonalInfoEditButton: UIButton!
+    
+    @IBOutlet weak var PaymentMethodsEditButton: UIButton!
+    
+    @IBOutlet weak var CardNumberText: UITextView!
+    
+    @IBOutlet weak var PasswordText: UITextView!
+    
+    @IBOutlet weak var UsernameText: UITextView!
+    
+    @IBOutlet weak var BirthdayText: UITextView!
+    
+    @IBOutlet weak var LocationText: UITextView!
+    
+    @IBOutlet weak var EmailText: UITextView!
+    
+    @IBOutlet weak var NameText: UITextView!
+    
+    
+    @IBOutlet weak var SearchButton: UIImageView!
+    
+    @IBOutlet weak var ReservationsButton: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
