042aedc202824ad11c06c227814504e281050640
nrewankar <66852090+nrewankar@users.noreply.github.com>
2020-08-15 13:39:35 -0400

    Add files via upload

---
+++ LoginPage/LoginPage/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Owner1 on 2020-08-12.
+//  Copyright © 2020 Reserv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var BackButton: UIButton!
+    
+    @IBOutlet weak var Username: UITextField!
+    
+    @IBOutlet weak var Password: UITextField!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var DontRememberPasswordButton: UIButton!
+    
+    @IBOutlet weak var LoginWithFacebookButton: UIButton!
+    
+    @IBOutlet weak var LoginWithGoogleButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
