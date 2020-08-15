3f1981010394217d11f7540fbf1199c7781c2e4c
nrewankar <66852090+nrewankar@users.noreply.github.com>
2020-08-15 13:59:08 -0400

    Add files via upload

---
+++ HomePage.updated/Assignment3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Owner1 on 2020-07-08.
+//  Copyright © 2020 Reserv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var SignInButton: UIButton!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
