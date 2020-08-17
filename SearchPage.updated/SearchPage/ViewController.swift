59f6604d285c4f3fed93b424538c9808e5f8a690
nrewankar <66852090+nrewankar@users.noreply.github.com>
2020-08-17 18:32:30 -0400

    Add files via upload

---
+++ SearchPage.updated/SearchPage/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SearchPage
+//
+//  Created by Owner1 on 2020-07-17.
+//  Copyright © 2020 Reserv. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var SearchBar: UITextField!
+    
+    @IBOutlet weak var Map: MKMapView!
+    
+    @IBOutlet weak var Item1: UIButton!
+    @IBOutlet weak var Item2: UIButton!
+    @IBOutlet weak var Item3: UIButton!
+    @IBOutlet weak var Item4: UIButton!
+    @IBOutlet weak var Item5: UIButton!
+    @IBOutlet weak var Item6: UIButton!
+    
+    @IBOutlet weak var HomeButton: UIImageView!
+    
+    @IBOutlet weak var ReservationsButton: UIImageView!
+    
+    @IBOutlet weak var AccountButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
