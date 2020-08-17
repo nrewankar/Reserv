8fffe4fc6e9c68884186004e17c1fa10b4fe9bba
nrewankar <66852090+nrewankar@users.noreply.github.com>
2020-08-17 16:35:20 -0400

    Add files via upload

---
+++ OrderConfirmationPage.updated/OrderConfirmationPage/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  OrderConfirmationPage
+//
+//  Created by Owner1 on 2020-07-29.
+//  Copyright © 2020 Reserv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var PersonalInfoButton: UIButton!
+    
+    @IBOutlet weak var BackButton: UIImageView!
+    
+    @IBOutlet weak var RestaurantNameText: UITextView!
+    
+    @IBOutlet weak var RestaurantPhoto: UIImageView!
+    
+    @IBOutlet weak var OrderSummaryText: UITextView!
+    
+    @IBOutlet weak var TotalAmountText: UITextView!
+    
+    @IBOutlet weak var EditButton: UIImageView!
+    
+    @IBOutlet weak var CardNumText: UITextView!
+    
+    
+    @IBOutlet weak var HomeButton: UIImageView!
+    
+    
+    @IBOutlet weak var SearchButton: UIImageView!
+    
+    
+    @IBOutlet weak var ReservationsButton: UIImageView!
+    
+    @IBOutlet weak var ReservationNumber: UITextView!
+    
+    @IBOutlet weak var ReservationTime: UITextView!
+    
+    @IBOutlet weak var NumPeople: UITextView!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
